d67f6f69aa4cbf3c3c46431f80e107b51e6a83ac
Molly <117697419+learningcoding2022@users.noreply.github.com>
2025-07-11 10:14:58 -0400

    added type to TodoForm

--- src/TodoForm.jsx
+++ src/TodoForm.jsx
@@ -2,7 +2,7 @@ const TodoForm = () => {
   return (
     <form>
       <label htmlFor="todoTitle">Todo</label>
-      <input id="todoTitle" />
+      <input id="todoTitle" type="text" />
       <button>Add Todo</button>
     </form>
   );
