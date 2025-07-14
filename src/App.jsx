1efdb87a57f64c3321ce2298275f9b52324460c3
Molly <117697419+learningcoding2022@users.noreply.github.com>
2025-07-14 13:49:29 -0400

    state for New Todos

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import './App.css';
 import TodoList from './ToDoList';
 import TodoForm from './TodoForm';
+import { useState } from 'react';
 
 function App() {
+  //create new state value that will hold a new todo
+  //this is creating a useState hook
+  const [newTodo, setNewTodo] = useState('Example Text');
   return (
     <div>
       <h1>My Todos</h1>
       <TodoForm />
+      <p>{newTodo}</p>
       <TodoList />
     </div>
   );
