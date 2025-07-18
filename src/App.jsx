138a42d5c429a000903d989ccc341e8d3d380452
Molly <117697419+learningcoding2022@users.noreply.github.com>
2025-07-18 12:17:56 -0400

    updated state, added addTodo function, and passed props to the return statement to be used

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,30 @@
+//app.jsx coordinates everything
+//acting as the parent
+import { useState } from 'react';
 import './App.css';
 import TodoList from './ToDoList';
 import TodoForm from './TodoForm';
-import { useState } from 'react';
 
 function App() {
   //create new state value that will hold a new todo
   //this is creating a useState hook
-  const [newTodo, setNewTodo] = useState('Example Text');
+  const [todoList, setTodoList] = useState([]);
+
+  function addTodo(title) {
+    const newTodo = {
+      title: title,
+      id: Date.now(),
+    };
+    //calling the updater funtions...saying heres the newtodo add it to the list
+    //this is where the actual updating is happening
+    setTodoList([...todoList, newTodo]);
+  }
+
   return (
     <div>
       <h1>My Todos</h1>
-      <TodoForm />
-      <p>{newTodo}</p>
-      <TodoList />
+      <TodoForm onAddTodo={addTodo} />
+      <TodoList todoList={todoList} />
     </div>
   );
 }
