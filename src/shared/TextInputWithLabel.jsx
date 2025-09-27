62478699eb69f885270772bc7d8c64d915ad8acc
Molly <117697419+learningcoding2022@users.noreply.github.com>
2025-09-27 17:07:48 -0400

    styled items in TextInputWith Label

--- src/shared/TextInputWithLabel.jsx
+++ src/shared/TextInputWithLabel.jsx
@@ -1,16 +1,26 @@
 import styled from 'styled-components';
 
+const StyledLabel = styled.label`
+  padding: 0.5rem;
+`;
+
+const StyledInput = styled.input`
+  padding: 0.5rem;
+  background-color: #f2e4cf;
+  border-radius: 0.5rem;
+`;
+
 function TextInputWithLabel({ elementId, labelText, onChange, ref, value }) {
   return (
     <>
-      <label htmlFor={elementId}>{labelText}</label>
-      <input
+      <StyledLabel htmlFor={elementId}>{labelText}</StyledLabel>
+      <StyledInput
         type="text"
         id={elementId}
         ref={ref}
         value={value}
         onChange={onChange}
-      ></input>
+      ></StyledInput>
     </>
   );
 }
