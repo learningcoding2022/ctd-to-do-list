47bfee6e74f9e7d6015f14aaf114ad46ab49be70
Molly <117697419+learningcoding2022@users.noreply.github.com>
2025-09-28 13:51:26 -0400

    created reducer, stated parameters, defined switch.case statements

--- src/todos.reducer.js
+++ src/todos.reducer.js
@@ -26,5 +26,51 @@ const initialState = {
     error: '',
 };
 
-export { initialState, actions };
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    case actions.fetchTodos:
+      return {
+        ...state,
+      };
+    case actions.loadTodos:
+      return {
+        ...state,
+      };
+    case actions.setLoadError:
+      return {
+        ...state,
+      }
+    case actions.startRequest:
+        return {
+        ...state,
+        }
+    case actions.addTodo:
+        return {
+        ...state,
+        }
+    case actions.endRequest:
+        return {
+        ...state,
+        }
+    case actions.updateTodo:
+        return {
+        ...state,
+        }
+    case actions.completeTodo:
+        return {
+        ...state,
+        }
+    case actions.revertTodo:
+        return {
+        ...state,
+        }
+    case actions.clearError:
+        return {
+        ...state,
+        }
+    default: return state;
+    }
+};
+
+export { initialState, actions, reducer };
 
