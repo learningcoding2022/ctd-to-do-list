63eecf75d5a4c5988bd3b7f4980f4e29efa48d57
Molly <117697419+learningcoding2022@users.noreply.github.com>
2025-09-28 17:40:43 -0400

    update Todo, completeTodo (optimistic UI), dismiss error button

--- src/todos.reducer.js
+++ src/todos.reducer.js
@@ -54,30 +54,68 @@ function reducer(state = initialState, action) {
     case actions.startRequest:
         return {
         ...state,
+        isSaving: true,
         }
-    case actions.addTodo:
-        return {
+    case actions.addTodo: {
+        const { id, fields } = action.records[0];
+        const savedTodo = { id, ...fields };
+      if (!fields.isCompleted) {
+        savedTodo.isCompleted = false;
+      }
+      return {
         ...state,
-        }
+
+        todoList: [...state.todoList, savedTodo],
+        isSaving: false,
+        };
+    }
     case actions.endRequest:
         return {
         ...state,
+        isLoading: false,
+        isSaving: false,
         }
-    case actions.updateTodo:
-        return {
+    case actions.revertTodo: 
+    case actions.updateTodo: {
+        const updatedTodos = state.todoList.map(function(todo) {
+            if (todo.id === action.editedTodo.id) {
+                if (action.error) {
+                return {...action.editedTodo, errorMessage: action.error.message};
+                }
+            return action.editedTodo;
+        }
+        return todo;
+        });
+
+        const updatedState = {
         ...state,
+        todoList: updatedTodos,
+        };
+
+        return updatedState;
         }
-    case actions.completeTodo:
-        return {
-        ...state,
+
+    case actions.completeTodo: {
+        const updatedTodos = state.todoList.map(function(todo) {
+        if (todo.id === action.id) {
+            return { ...todo, isCompleted: true };
+        } else {
+            return todo;
         }
-    case actions.revertTodo:
-        return {
-        ...state,
-        }
+        });
+
+        const updatedState = {
+            ...state,
+            todoList: updatedTodos, 
+        };
+
+        return updatedState;
+    }
+  
     case actions.clearError:
         return {
         ...state,
+        error: '',
         }
     default: return state;
     }
